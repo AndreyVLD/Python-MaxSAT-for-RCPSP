6f6f365717e61ee3c687e85d6eb71891b0c2b8b2
Andrei Vlad Nicula <94231732+AndreyVLD@users.noreply.github.com>
2023-10-17 00:55:10 +0200

    Added Completion Clauses Si

--- main.py
+++ main.py
@@ -3,26 +3,40 @@ from pysat.formula import WCNF
 from DataParser import TaskDataParser
 
 # Reading the input RCPSP file
-file_path = input("Please input the file path of the data file: ")
-parser = TaskDataParser(file_path)
+# file_path = input("Please input the file path of the data file: ")
+parser = TaskDataParser("Data/Q3_1.dzn")
 parser.read_data_from_file()
 parser.print_data()
 
+# (Activity, Time) to literal mapping
+activity_time_mapping = {}
+T = sum(parser.d) + max(parser.d) + 1  # Time horizon
+literal_counter = 1
+
+for i in range(1, parser.N_TASKS + 1):
+    di = parser.d[i - 1]
+    for t in range(0, T - di + 1):
+        activity_time_mapping[(i, t)] = literal_counter
+        literal_counter += 1
+
 # Creating the corresponding WCNF formula
-cnf = WCNF()
+wcnf = WCNF()
+
+for i in range(1, parser.N_TASKS + 1):
+    di = parser.d[i - 1]
+    clause = []
+    for t in range(0, T - di + 1):
+        xi_t = activity_time_mapping[(i, t)]
+        clause.append(xi_t)
+    wcnf.append(clause)
 
 # Printing to file as DIMACS
-cnf.to_file(file_path.replace(".dzn", ".wcnf"))
-
-
-
-
-
+wcnf.to_file("Data/Q3_1.wcnf")  # file_path.replace(".dzn", ".wcnf"))
 # Reads the formula from a file
 # cnf = WCNF(from_file='Data/test1.wcnf')
 
 # Print the soft and hard clauses
-# print("Soft: "+cnf.soft.__str__()+"\n"+"Hard: "+cnf.hard.__str__())
+# print("Soft: " + cnf.soft.__str__() + "\n" + "Hard: " + cnf.hard.__str__())
 
 # Solve the formula
 # rc2 = RC2(cnf)
