ca11e544f7f71838b18eb7ddf6a93fb61d373315
Andrei Vlad Nicula <94231732+AndreyVLD@users.noreply.github.com>
2023-11-20 20:02:06 +0100

    Spacing format

--- main.py
+++ main.py
@@ -18,11 +18,10 @@ parser = TaskDataParser(file_path)
 parser.read_data_from_file()
 parser.print_data()
 
-
-activity_time_mapping = {}    # (Activity, Time) started at Time
-during_activity = {}    # (Activity, DuringTime) is on during DuringTime
-T = sum(parser.d) + max(parser.d) + 1   # Time horizon
-literal_counter = 1    # Counter for the literals in the WCNF formula
+activity_time_mapping = {}  # (Activity, Time) started at Time
+during_activity = {}  # (Activity, DuringTime) is on during DuringTime
+T = sum(parser.d) + max(parser.d) + 1  # Time horizon
+literal_counter = 1  # Counter for the literals in the WCNF formula
 
 # (Activity, Time) to literal mapping
 for i in range(1, parser.N_TASKS + 1):
@@ -54,51 +53,49 @@ for i in range(1, parser.N_TASKS + 1):
 for t in range(T):
     literals_sum = [activity_time_mapping[(i, t)] for i in range(1, parser.N_TASKS + 1) if
                     (i, t) in activity_time_mapping]
-    literals_sum_clause = CardEnc.equals(lits=literals_sum, bound=1,top_id=literal_counter)
+    literals_sum_clause = CardEnc.equals(lits=literals_sum, bound=1, top_id=literal_counter)
 
     # Calculating the highest variable to avoid collisions from the auxiliary variables
     highest_variable = max(abs(lit) for clause in literals_sum_clause for lit in clause)
-    literal_counter=highest_variable
+    literal_counter = highest_variable
 
     wcnf.extend(literals_sum_clause)
 
 # Resource Clause C_i
-for i in range(1,parser.N_TASKS+1):
-    di = parser.d[i-1]
-    for t in range(0,T-di+1):
-        for u in range(t,t+di-1):
-            wcnf.append([-activity_time_mapping[(i,t)],during_activity[(i,u)]])
+for i in range(1, parser.N_TASKS + 1):
+    di = parser.d[i - 1]
+    for t in range(0, T - di + 1):
+        for u in range(t, t + di - 1):
+            wcnf.append([-activity_time_mapping[(i, t)], during_activity[(i, u)]])
 
 # Resource Clause R_k_t
-for t in range(0,T):
+for t in range(0, T):
     lit_R = [during_activity[(i, t)] for i in range(1, parser.N_TASKS + 1) if
-                    (i, t) in during_activity]
+             (i, t) in during_activity]
 
-    wcnf_r = PBEnc.atmost(lit_R,parser.rr,bound=parser.CAPACITY,top_id=literal_counter)
+    wcnf_r = PBEnc.atmost(lit_R, parser.rr, bound=parser.CAPACITY, top_id=literal_counter)
 
     wcnf.extend(wcnf_r.clauses)
     highest_variable = max(abs(lit) for clause in wcnf_r for lit in clause)
 
     literal_counter = highest_variable
 
-
-
 # Precedence Clause P_i_j
 for j in range(parser.N_TASKS):
 
     # For every task we get the dependents
     dependents = parser.suc[j]
-    dj  = parser.d[j]
+    dj = parser.d[j]
 
     # Vector of dependents
     z_i_t = []
-    if len(dependents) > 0:    # If there are dependents
-        for t in range(0,T-dj+1):  # For every time step
-            for i in dependents:        # For every dependent
-                di = parser.d[i-1]
-                for u in  range(0,t-di):    # Computing z_i_t
-                    z_i_t.append(activity_time_mapping[(i,u)])
-                z_i_t.append(-activity_time_mapping[(j+1,t)])     # Adding -x_j_t
+    if len(dependents) > 0:  # If there are dependents
+        for t in range(0, T - dj + 1):  # For every time step
+            for i in dependents:  # For every dependent
+                di = parser.d[i - 1]
+                for u in range(0, t - di):  # Computing z_i_t
+                    z_i_t.append(activity_time_mapping[(i, u)])
+                z_i_t.append(-activity_time_mapping[(j + 1, t)])  # Adding -x_j_t
                 wcnf.append(z_i_t)  # Appending the clause with AND
                 z_i_t = []
 
@@ -116,8 +113,6 @@ print(solution)
 for i in range(1, parser.N_TASKS + 1):
     di = parser.d[i - 1]
     for t in range(0, T - di + 1):
-        if solution[activity_time_mapping[(i, t)]-1] > 0:
+        if solution[activity_time_mapping[(i, t)] - 1] > 0:
             print(f"Task {i} starts at time {t}")
             break
-
-
